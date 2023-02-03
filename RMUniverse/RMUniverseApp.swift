4ceeb79c1de4648c330e3e0d85bef91955abfa7e
Aleksandr Kretov <kretov.1988@gmail.com>
2023-02-03 12:44:12 +0500

    Initial Commit

---
+++ RMUniverse/RMUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMUniverseApp.swift
+//  RMUniverse
+//
+//  Created by Aleksandr Kretov on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct RMUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
